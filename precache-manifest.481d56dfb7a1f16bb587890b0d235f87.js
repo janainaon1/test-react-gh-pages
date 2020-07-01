15680710bb4115431480943b7ca1465c6c8ee305
Janaina Oliveira <janainaon1@gmail.com>
2020-06-30 22:10:12 -0300

    Updates

---
+++ precache-manifest.481d56dfb7a1f16bb587890b0d235f87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c12d3adb95282c6e2dd564a10b881b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b488fade9b74c61d92",
+    "url": "/static/css/main.9a95e84a.chunk.css"
+  },
+  {
+    "revision": "c10719053388db7915dd",
+    "url": "/static/js/2.e9dca4e3.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.e9dca4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b488fade9b74c61d92",
+    "url": "/static/js/main.529e6826.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
